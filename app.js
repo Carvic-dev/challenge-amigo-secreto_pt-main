16d7e3a8ae3581245b83a3c16385ff0412dfafba
Carlos Victor <carllos.victor@hotmail.com>
2025-02-05 12:07:05 -0300

    Adição de confetes ao sortear um nome

--- app.js
+++ app.js
@@ -55,6 +55,12 @@ function sortFriend() {
 
 
     document.getElementById('resultado').innerHTML = `Amigo sorteado: ${selectedFriend}`;
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
 };
 
 function reset() {
